b0c05bddd6827a813867faaada3454faa7eeabd3
svnday <40030806+svnday@users.noreply.github.com>
2022-03-13 21:18:54 -0500

    routes created for User, Thought, and Reactions. Virtuals created to count reactions and friends + getters and dateFormat implementation.

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please provide a valid email!']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = User;
