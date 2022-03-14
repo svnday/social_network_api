2a4ce637714b2eb9d12ef32b5199bc8097031037
svnday <40030806+svnday@users.noreply.github.com>
2022-03-14 09:39:23 -0500

    capital letter

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -76,7 +76,7 @@ const userController = {
     },
 
     removeFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendid } }, { runValidators: true })
+        User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendId } }, { runValidators: true })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No user found with this id' });
